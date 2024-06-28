8cef089adf88a75737a752c0bec9efe79fcecc71
ansaryshohan <ansarye2083@gmaail.com>
2024-06-28 23:03:19 +0600

    this is the second project for themezone where star component is done properly

--- src/components/shared/NavBar.jsx
+++ src/components/shared/NavBar.jsx
@@ -1,15 +1,28 @@
-import React, { Component } from "react";
+import {useState,useEffect} from "react"
 import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import logo from "../../Assets/Logo.png";
 
-class NavBar extends Component {
-  render() {
+export default function NavBar(){
+  const [scrollColor, setScrollColor] = useState(false);
+  const scrollColorChange = () => {
+    if (window.scrollY >= 80) {
+      setScrollColor(true);
+    } else setScrollColor(false);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", scrollColorChange);
+    return () => {
+      return window.removeEventListener("scroll", scrollColorChange);
+    };
+  }, []);
+
+
     return (
       <div>
         <Navbar
           expand="lg"
-          className="bg-transparent  position-fixed w-100 navbar-section"
+          className={`${scrollColor?"bg-warning-subtle":"bg-body-tertiary"} position-fixed w-100 navbar-section`}
         >
           <Container className="d-flex">
             <div className="d-flex align-items-center justify-content-center">
@@ -24,7 +37,7 @@ class NavBar extends Component {
                 className="middle-nav me-auto ms-5 my-2 my-lg-0 d-flex flex-column flex-md-row align-items-center gap-3"
                 navbarScroll
               >
-                <Link to="/home" >Home</Link>
+                <Link to="/home">Home</Link>
                 <Link to="/about">About</Link>
                 <Link to="/blogs">Blogs</Link>
                 <Link to="/services">Services</Link>
@@ -32,15 +45,26 @@ class NavBar extends Component {
               </Nav>
               <div className="d-flex flex-column flex-md-row align-items-center gap-3">
                 {" "}
-                <div><Button variant="">Login</Button></div>
-                <div> <Button variant="danger">Search</Button></div>
+                <div>
+                  <Button
+                    variant=""
+                    onClick={() => {
+                      this.setState({ scrollNew: 12 });
+                      console.log(this.state.scrollNew);
+                    }}
+                  >
+                    Login
+                  </Button>
+                </div>
+                <div>
+                  {" "}
+                  <Button variant="danger">Search</Button>
+                </div>
               </div>
             </Navbar.Collapse>
           </Container>
         </Navbar>
       </div>
     );
-  }
 }
 
-export default NavBar;
